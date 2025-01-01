e4bf277d854caef851fb6b89e1c58f13a3a2c41f
rudugampola <ravindu365@gmail.com>
2024-12-31 19:01:43 -0800

    added gitignore

--- camp/views.py
+++ camp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from datetime import datetime
 from .camping import run_campsite_check
+import json
+from django.http import JsonResponse
+from django.conf import settings
+import os
+import ijson
 
-# TODO: Add a bootstrap notification if no data is entered and submitted. Also add required fields to submit form.
+# TODO: Add a rating for the campsite
 
 
 class bcolors:
@@ -24,10 +28,10 @@ def select_camp(request):
         start_date_str = request.POST.get("start_date")
         end_date_str = request.POST.get("end_date")
 
-        # The campsite_id has comma-separated values, so we need to split them into a list of integers
-        campsite_id = int(request.POST.get("campsite_id"))
+        # The parks has comma-separated values, so we need to split them into a list of strings
+        parks = request.POST.get("parks").strip().split(',')
 
-        nights = request.POST.get("nights")
+        # nights = request.POST.get("nights")
 
         # The dates are in the format "YYYY-MM-DD", so we can parse them into datetime objects
         start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
@@ -41,20 +45,38 @@ def select_camp(request):
 
         # Pass the 'show_campsite_info' value as "true" if checked
         if show_campsite_info:
-            output, has_availabilities, campsite_info = run_campsite_check(
-                [campsite_id],
+            output, has_availabilities, raw_campsite_info = run_campsite_check(
+                parks,
                 start_date,
                 end_date,
-                nights=int(nights),
+                # nights=int(nights),
                 show_campsite_info=show_campsite_info,
                 weekends_only=weekends_only
             )
+
+            # Convert raw_campsite_info to a list of dictionaries for easier iteration in the template
+            campsite_info = []
+            for park_id, park_info in raw_campsite_info.items():
+                park_data = {
+                    'park_id': park_id,
+                    'available_sites': park_info[0],
+                    'total_sites': park_info[1],
+                    'sites': [],
+                    'park_name': park_info[3]
+                }
+                for site_id, dates in park_info[2].items():
+                    site_data = {
+                        'site_id': site_id,
+                        'dates': dates
+                    }
+                    park_data['sites'].append(site_data)
+                campsite_info.append(park_data)
         else:
             output, has_availabilities = run_campsite_check(
-                [campsite_id],
+                parks,
                 start_date,
                 end_date,
-                nights=int(nights),
+                # nights=int(nights),
                 show_campsite_info=show_campsite_info,
                 weekends_only=weekends_only
             )
@@ -64,8 +86,8 @@ def select_camp(request):
         context = {
             'start_date': start_date_str,
             'end_date': end_date_str,
-            'campsite_id': campsite_id,
-            'nights': nights,
+            'parks': parks,
+            # 'nights': nights,
             'output': output,
             'has_availabilities': has_availabilities,
             'show_campsite_info': show_campsite_info,
@@ -74,12 +96,85 @@ def select_camp(request):
             'title': 'Camp Reservation Result'
         }
 
-        # print("Context:", context)
-        print(bcolors.OKGREEN + "Output: " +
-              output + bcolors.ENDC)
+        # Print the output
+        print(bcolors.OKGREEN + "Output: " + output + bcolors.ENDC)
+        # Print the campsite_info
+        # print(campsite_info)
 
         # Return the rendered template with the context data
         return render(request, 'camp/camp_result.html', context)
 
     # Render the select_camp.html template
     return render(request, 'camp/select_camp.html', {'title': 'Select Camp'})
+
+
+def show_parks(request):
+    # Define the file path
+    file_path = os.path.join(settings.BASE_DIR, 'camp',
+                             'other', 'RecAreas_API_v1.json')
+
+    # Get the search query from the request
+    search_query = request.GET.get('q', '').lower()
+
+    # Initialize an empty list to hold the filtered parks
+    parks = []
+
+    # Read the JSON data from the file using ijson
+    try:
+        with open(file_path, 'r', encoding='utf-8') as json_file:
+            # Use ijson to parse the JSON incrementally
+            objects = ijson.items(json_file, 'RECDATA.item')
+            for obj in objects:
+                if isinstance(obj, dict) and search_query in obj.get('RecAreaName', '').lower():
+                    parks.append({
+                        'id': obj.get('RecAreaID', 'N/A'),
+                        'name': obj.get('RecAreaName', 'N/A'),
+                        'latitude': obj.get('RecAreaLatitude', 'N/A'),
+                        'longitude': obj.get('RecAreaLongitude', 'N/A')
+                    })
+
+        return render(request, 'camp/show_parks.html', {'parks': parks, 'search_query': search_query})
+    except FileNotFoundError:
+        return JsonResponse({'error': 'RecAreas_API_v1.json file not found'}, status=404)
+    except ijson.JSONError:
+        return JsonResponse({'error': 'Error decoding JSON data'}, status=500)
+    except UnicodeDecodeError as e:
+        return JsonResponse({'error': f'Unicode decode error: {str(e)}'}, status=500)
+    except Exception as e:
+        return JsonResponse({'error': f'Unexpected error: {str(e)}'}, status=500)
+
+
+def show_campsites(request):
+    # Define the file path
+    file_path = os.path.join(settings.BASE_DIR, 'camp',
+                             'other', 'Campsites_API_v1.json')
+
+    # Get the search query from the request
+    search_query = request.GET.get('q', '').lower()
+
+    # Initialize an empty list to hold the filtered campsites
+    campsites = []
+
+    # Read the JSON data from the file using ijson
+    try:
+        with open(file_path, 'r', encoding='utf-8') as json_file:
+            # Use ijson to parse the JSON incrementally
+            objects = ijson.items(json_file, 'RECDATA.item')
+            for obj in objects:
+                if isinstance(obj, dict) and search_query in obj.get('FacilityID', '').lower():
+                    campsites.append({
+                        'id': obj.get('CampsiteID', 'N/A'),
+                        'name': obj.get('CampsiteName', 'N/A'),
+                        'loop': obj.get('Loop', 'N/A'),
+                        'facility_id': obj.get('FacilityID', 'N/A'),
+                    })
+
+        return render(request, 'camp/show_campsites.html', {'campsites': campsites, 'search_query': search_query})
+    except FileNotFoundError:
+        return JsonResponse({'error': 'Campsites_API_v1.json file not found'}, status=404)
+    except ijson.JSONError:
+        return JsonResponse({'error': 'Error decoding JSON data'}, status=500)
+    except UnicodeDecodeError as e:
+        return JsonResponse({'error': f'Unicode decode error: {str(e)}'}, status=500)
+    except Exception as e:
+        return JsonResponse({'error': f'Unexpected error: {str(e)}'}, status=500)
